cee076fd43f2b667cf04fdb3c1b190af93234995
David <60241754+drvillota@users.noreply.github.com>
2024-03-11 10:20:43 -0500

    Script to convert csv to json using pandas lib

---
+++ csv-to-json-pnd.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import time
+
+def csv_to_json_pnd(csvFilePath, jsonFilePath):
+    # Read csv file
+    df = pd.read_csv (csvFilePath)
+    df.to_json (jsonFilePath)
+
+# Csv data file path
+csvFilePath = r"radar.csv"
+# Json data file path
+jsonFilePath = r"radar_pnd.json"
+
+
+start = time.perf_counter()
+csv_to_json_pnd(csvFilePath, jsonFilePath)
+finish = time.perf_counter()
+
+print(f"Conversion completed successfully in {finish - start:0.4f} seconds")
